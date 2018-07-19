98aaf58b8bb266ae7ce396ab5eee697ff370afe8
shinjihattori-ffs <38636994+shinjihattori-ffs@users.noreply.github.com>
2018-07-19 15:50:20 +0900

    リストビューにパスが無い状態で検索ボタンをクリックした場合のエラーメッセージ後の挙動を修正（再）

--- PhotoFrameApp/PhotoFrameForm.cs
+++ PhotoFrameApp/PhotoFrameForm.cs
@@ -114,6 +114,15 @@ namespace PhotoFrameApp
             }
 
             // フォルダパスを引数にとって、コントローラーに渡す
+            else if (controller.ExecuteSearchFolder(folderPath).Count() == 0)
+            {
+                MessageBox.Show("写真が存在しません");
+            }
+            else if (controller.ExecuteSearchFolder(folderPath) == null)
+            {
+                MessageBox.Show("フォルダが存在しません");
+            }
+            // フォルダパスを引数にとって、コントローラーに渡す
             else
             {
                 this.searchedPhotos = controller.ExecuteSearchFolder(folderPath);
@@ -122,17 +131,7 @@ namespace PhotoFrameApp
                 RenewPhotoListView();
             }
 
-            // フォルダパスを引数にとって、コントローラーに渡す
-            if (controller.ExecuteSearchFolder(folderPath).Count() == 0)
-            {
-                MessageBox.Show("写真が存在しません");
-            }
-            else if (controller.ExecuteSearchFolder(folderPath) == null)
-            {
-                MessageBox.Show("フォルダが存在しません");
-            }
 
-            
         }
 
         /// <summary>
