3433ea8f7f4dd306ee48aad2d982b1def8f39f6a
rinkokomiya-ffs <rinko.komiya@fujifilm.com>
2018-07-24 13:31:52 +0900

    集約例外ハンドラの実装

--- PhotoFrameApp/Program.cs
+++ PhotoFrameApp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -14,9 +15,54 @@ namespace PhotoFrameApp
         [STAThread]
         static void Main()
         {
+            // 集約例外ハンドラ
+            // ThreadExceptionイベント・ハンドラを登録する
+            Application.ThreadException += new
+              ThreadExceptionEventHandler(Application_ThreadException);
+
+            // UnhandledExceptionイベント・ハンドラを登録する
+            Thread.GetDomain().UnhandledException += new
+              UnhandledExceptionEventHandler(Application_UnhandledException);
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new PhotoFrameForm());
+        }
+
+        private static void Application_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            try
+            {
+                Exception ex = (Exception)e.ExceptionObject;
+
+                //エラー処理
+                MessageBox.Show(
+                    "エラーが発生しました。アプリを終了します。\n開発元にお知らせください。\n\n" +
+                    "【エラー内容】\n" + ex.Message + "\n\n" +
+                    "【スタックトレース】\n" + ex.StackTrace, "警告", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+            finally
+            {
+                Application.Exit();
+            }
+        }
+
+        private static void Application_ThreadException(object sender, ThreadExceptionEventArgs e)
+        {
+            try
+            {
+                Exception ex = e.Exception;
+
+                //エラー処理
+                MessageBox.Show(
+                    "エラーが発生しました。アプリを終了します。\n開発元にお知らせください。\n\n" +
+                    "【エラー内容】\n" + ex.Message + "\n\n" +
+                    "【スタックトレース】\n" + ex.StackTrace, "警告", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+            finally
+            {
+                Application.Exit();
+            }
         }
     }
 }
